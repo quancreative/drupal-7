fe784c03261e3cff138a567f71822a3f5303b4df
Quan Ngo <quancreative@gmail.com>
2012-06-11 22:58:23 -0400

    Update to 7.14.

--- sites/all/themes/vietfest/page--front.tpl.php
+++ sites/all/themes/vietfest/page--front.tpl.php
@@ -42,10 +42,9 @@ include 'includes/top-nav.php';
 
 				<div class="box-6">
 					<h2>Location</h2>
-					<iframe width="450" height="409" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;safe=active&amp;ie=UTF8&amp;q=george+mason+university&amp;fb=1&amp;gl=us&amp;hq=george+mason+university&amp;hnear=0x89b645fed4c0731d:0xbf8c913c6a96c41e,Fairfax,+VA+22033&amp;cid=0,0,8480217559846663912&amp;t=m&amp;ll=38.831518,-77.304783&amp;spn=0.00585,0.009656&amp;z=16&amp;iwloc=A&amp;output=embed"></iframe>
-					<br />
-					<small><a href="http://maps.google.com/maps?hl=en&amp;safe=active&amp;ie=UTF8&amp;q=george+mason+university&amp;fb=1&amp;gl=us&amp;hq=george+mason+university&amp;hnear=0x89b645fed4c0731d:0xbf8c913c6a96c41e,Fairfax,+VA+22033&amp;cid=0,0,8480217559846663912&amp;t=m&amp;ll=38.831518,-77.304783&amp;spn=0.00585,0.009656&amp;z=16&amp;iwloc=A&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+					<iframe width="450" height="409" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?hl=en&amp;q=4500+Patriot+Circle,++Fairfax,+VA+22030&amp;ie=UTF8&amp;hq=&amp;hnear=4500+Patriot+Cir,+Fairfax,+Virginia+22030&amp;gl=us&amp;t=m&amp;z=14&amp;iwloc=A&amp;ll=38.826745,-77.30975&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?hl=en&amp;q=4500+Patriot+Circle,++Fairfax,+VA+22030&amp;ie=UTF8&amp;hq=&amp;hnear=4500+Patriot+Cir,+Fairfax,+Virginia+22030&amp;gl=us&amp;t=m&amp;z=14&amp;iwloc=A&amp;ll=38.826745,-77.30975&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small>
 				</div>
+				
 				<div class="box-6 box-last">
 					<h2>Info</h2>
 					<address>George Mason University<br />
@@ -112,7 +111,7 @@ include 'includes/top-nav.php';
 		</div>
 	</div><!-- end .sponsor-container -->
 	<div class="partner-container container-12 clearfix">
-		<div class="grid-12 clearfix">
+		<div class="grid-12 clearfix">    
 			<h2>Partners</h2>
 			<div class="partner">
 				<img src="./sites/all/themes/vietfest/images/sponsors/Partner Logos.png" alt="Sponsors" usemap="#partnerMap" />
